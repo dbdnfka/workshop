65631ce6d45f4e285356992aaac9f5161ac55d12
dbdnfka <dbdnfka0@naver.com>
2023-03-19 19:22:17 +0900

    version-0.0.18

--- src/main/java/com/Woo/Ram/controller/ArticleController.java
+++ src/main/java/com/Woo/Ram/controller/ArticleController.java
@@ -9,9 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -32,7 +30,8 @@ public class ArticleController {
         return "articles/new";
     }
 
-    @GetMapping("/")
+
+    @RequestMapping(value= "/", method = RequestMethod.GET)
     public String index2(Model model){
 //        List<Article> articleEntityList = articleRepository.findAll();
 //
